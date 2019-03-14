4bf227dfd41730773fd23649507a8cbc09f3201e
Tiago Reis <nuno-tiago-reis@outlook.pt>
2019-03-14 13:48:09 +0000

    Refactoring:
    Renamed the source and test folders.

--- Kindly/Kindly.SPA/tests/protractor.conf.js
+++ Kindly/Kindly.SPA/tests/protractor.conf.js
@@ -9,14 +9,14 @@ exports.config =
 	allScriptsTimeout: 11000,
 	specs:
 	[
-		'./src/**/*.e2e-spec.ts'
+		'./source/**/*.e2e-spec.ts'
 	],
 	capabilities:
 	{
 		'browserName': 'chrome'
 	},
 	directConnect: true,
-	baseUrl: 'http://localhost:4200/',
+	baseUrl: 'https://localhost:44351/',
 	framework: 'jasmine',
 	jasmineNodeOpts:
 	{
